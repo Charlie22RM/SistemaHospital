8e46138aceb56ab486b2ebf81a10eab8b08a5091
cristopher-gomez-m <cristopher.gomezm@ug.edu.ec>
2023-06-06 15:52:43 -0500

    agregada la validacion del formulario

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  validateFrm!: FormGroup;
 
-  
-  constructor(private router: Router) { }
+  constructor(private router: Router, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.validateFrm = this.fb.group({
+      email: [null, [Validators.required,Validators.email]],
+      password: [null, [Validators.required]],
+    });
+  }
+
+
+  submitForm(){
+    if(this.validateFrm.valid){
+      this.ingresar();
+    }else {
+      Object.values(this.validateFrm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
   }
 
   ingresar() {
-  
+    console.log('prueba');
   }
 
   registrar() {
     this.router.navigate(['/register']);
   }
-
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
-    AngularMaterialModule
-  ]
+    AngularMaterialModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    NgIf,
+  ],
 })
-export class LoginModule { }
+export class LoginModule {}
