216a2d129cad536c7d7b06c3f5ae60febd88001b
cristopher-gomez-m <cristopher.gomezm@ug.edu.ec>
2023-06-07 13:26:28 -0500

    agreggado validacion de ui en register

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environments';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +18,14 @@ export class AuthService {
     });
     let options = { headers };
     return this.http.post<any>(url,paramsData,options);
+  }
+
+  register(user:User){
+    let url = `${environment.apiUrl}auth/register`;
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    let options = { headers };
+    return this.http.post<any>(url,user,options);
   }
 }
