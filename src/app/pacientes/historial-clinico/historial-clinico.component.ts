19a9a224a9881bd19e3277c310d146aa04ceeb94
Charlie22RM <57648343+Charlie22RM@users.noreply.github.com>
2023-06-12 10:13:53 -0500

    Pacientes historial terminado

--- src/app/pacientes/historial-clinico/historial-clinico.component.ts
+++ src/app/pacientes/historial-clinico/historial-clinico.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-historial-clinico',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./historial-clinico.component.css']
 })
 export class HistorialClinicoComponent {
+  enfermedades = this._formBuilder.group({
+    diabetes: false,
+    enf_tiroideas: false,
+    hip_arterial: false,
+    cardiopatias: false,
+    traumatismos: false,
+    cancer: false,
+    otros: false
 
+  })
+
+  constructor(private router: Router, private _formBuilder: FormBuilder) { }
 }
--- src/app/pacientes/historial-clinico/historial-clinico.module.ts
+++ src/app/pacientes/historial-clinico/historial-clinico.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, JsonPipe, NgFor } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { AngularMaterialModule } from 'src/app/angular-material/angular-material.module';
 import { HistorialClinicoComponent } from './historial-clinico.component';
 import {MatRadioModule} from '@angular/material/radio';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 
 
@@ -21,7 +23,9 @@ import {MatRadioModule} from '@angular/material/radio';
     MatRadioModule,
     MatFormFieldModule,
     MatInputModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatGridListModule,
+   MatCheckboxModule, JsonPipe
   ]
 })
 export class HistorialClinicoModule { }
