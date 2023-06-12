f03b8976b0eedec59e6f9fc9cb03d25a47dbe6bd
cristopher-gomez-m <cristopher.gomezm@ug.edu.ec>
2023-06-12 14:05:52 -0500

    medicos ve pacientes

--- src/app/medicos/pacientes/pacientes.component.ts
+++ src/app/medicos/pacientes/pacientes.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 export interface DatosPacientes{
-  cedula: string;
+  id:number,
+  email: string;
   nombre: string;
   apellido: string;
   direccion: string;
 }
 
 const ELEMENT_DATA: DatosPacientes[] = [
-
+  {id:1,email:"prueba",nombre:"al",apellido:"casas",direccion:"su casa"},
 ];
 @Component({
   selector: 'app-pacientes',
@@ -17,7 +19,23 @@ const ELEMENT_DATA: DatosPacientes[] = [
 })
 export class PacientesComponent {
 
-  displayedColumns: string[] = ['cedula', 'nombre', 'apellido', 'direccion'];
+  constructor(
+    private router: Router,
+    ){}
+  displayedColumns: string[] = [
+    'id',
+    'email',
+    'nombre',
+    'apellido',
+    'direccion',
+    'actions',
+  ];
   dataSource = ELEMENT_DATA;
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    //this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
 }
