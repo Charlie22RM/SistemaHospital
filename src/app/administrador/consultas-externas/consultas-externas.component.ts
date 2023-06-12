00b41e649170fbf64e8195f3cc6d8239a750640c
Charlie22RM <57648343+Charlie22RM@users.noreply.github.com>
2023-06-12 15:16:09 -0500

    Consultas

--- src/app/administrador/consultas-externas/consultas-externas.component.ts
+++ src/app/administrador/consultas-externas/consultas-externas.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { Consultorio } from 'src/app/models/consultorio';
+import { UserDisplay } from 'src/app/models/user';
+import { ConsultorioService } from 'src/app/services/consultorio.service';
+import { MedicoDisplay } from 'src/app/models/medico';
+import { MedicoService } from 'src/app/services/medico.service';
 
 @Component({
   selector: 'app-consultas-externas',
@@ -7,6 +14,34 @@ import { Component } from '@angular/core';
 
 })
 
-export class ConsultasExternasComponent {
+export class ConsultasExternasComponent implements OnInit {
+  data!: Consultorio[];
+  displayedColumns: string[] = [
+    'id',
+    'especialidad',
+    'nombre_medico',
+    'apellido_medico',
+  ];
+
+
+  dataSource = new MatTableDataSource<Consultorio>();;
+  filterValue: string = '';
+  constructor(
+    private router: Router,
+    private consultorioService: ConsultorioService
+  ) { }
+  ngOnInit(): void {
+    this.consultorioService.getAll().subscribe(
+      {
+        next: async (res) => {
+          console.log(res);
+          this.data = res;
+          this.dataSource = new MatTableDataSource(this.data);
+        },
+      }
+    );
+  }
+
+
 
 }
