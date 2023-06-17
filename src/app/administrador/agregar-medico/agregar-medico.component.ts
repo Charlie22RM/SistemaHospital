4ed15fb3ae99509710357d800039b452d6bc18c8
cristopher-gomez-m <cristopher.gomezm@ug.edu.ec>
2023-06-17 17:10:09 -0500

    arreglo

--- src/app/administrador/agregar-medico/agregar-medico.component.ts
+++ src/app/administrador/agregar-medico/agregar-medico.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {  FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatSnackBar, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { MedicoService } from 'src/app/services/medico.service';
